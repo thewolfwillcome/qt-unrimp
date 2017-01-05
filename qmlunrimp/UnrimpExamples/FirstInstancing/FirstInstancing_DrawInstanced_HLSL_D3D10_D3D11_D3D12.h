5ba82d3478ea43bcea0d07b3492c7732e9898a30
Stephan Wezel <s.wezel@web.de>
2017-01-05 21:37:07 +0100

    added android manifest

---
+++ qmlunrimp/UnrimpExamples/FirstInstancing/FirstInstancing_DrawInstanced_HLSL_D3D10_D3D11_D3D12.h
@@ -0,0 +1,88 @@
+/*********************************************************\
+ * Copyright (c) 2012-2016 Christian Ofenberg
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+\*********************************************************/
+
+
+//[-------------------------------------------------------]
+//[ Shader start                                          ]
+//[-------------------------------------------------------]
+#if !defined(RENDERER_NO_DIRECT3D10) || !defined(RENDERER_NO_DIRECT3D11) || !defined(RENDERER_NO_DIRECT3D12)
+if (0 == strcmp(renderer->getName(), "Direct3D10") || 0 == strcmp(renderer->getName(), "Direct3D11") || 0 == strcmp(renderer->getName(), "Direct3D12"))
+{
+
+
+//[-------------------------------------------------------]
+//[ Define helper macro                                   ]
+//[-------------------------------------------------------]
+#define STRINGIFY(ME) #ME
+
+
+//[-------------------------------------------------------]
+//[ Vertex shader source code                             ]
+//[-------------------------------------------------------]
+// One vertex shader invocation per vertex
+vertexShaderSourceCode = STRINGIFY(
+// Attribute input/output
+struct VS_INPUT
+{
+	float2 Position   : POSITION;	// Clip space vertex position as output, left/bottom is (-1,-1) and right/top is (1,1)
+	uint   InstanceID : SV_INSTANCEID;
+};
+struct VS_OUTPUT
+{
+	float4 Position : SV_POSITION;	// Clip space vertex position as output, left/bottom is (-1,-1) and right/top is (1,1)
+};
+
+// Programs
+VS_OUTPUT main(VS_INPUT input)
+{
+	// Pass through the clip space vertex position, left/bottom is (-1,-1) and right/top is (1,1)
+	VS_OUTPUT output;
+	output.Position = float4(input.Position.x, input.Position.y - float(input.InstanceID), 0.0f, 1.0f);
+	return output;
+}
+);	// STRINGIFY
+
+
+//[-------------------------------------------------------]
+//[ Fragment shader source code                           ]
+//[-------------------------------------------------------]
+// One fragment shader invocation per fragment
+// "pixel shader" in Direct3D terminology
+fragmentShaderSourceCode = STRINGIFY(
+// Programs
+float4 main(float4 Position : SV_POSITION) : SV_TARGET
+{
+	// Return blue
+	return float4(0.0f, 0.0f, 1.0f, 1.0f);
+}
+);	// STRINGIFY
+
+
+//[-------------------------------------------------------]
+//[ Undefine helper macro                                 ]
+//[-------------------------------------------------------]
+#undef STRINGIFY
+
+
+//[-------------------------------------------------------]
+//[ Shader end                                            ]
+//[-------------------------------------------------------]
+}
+else
+#endif
